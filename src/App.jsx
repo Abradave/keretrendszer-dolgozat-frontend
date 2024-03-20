48e6983256ac83b0c0caa3943cc37d95c926ead9
Abradave <109905261+Abradave@users.noreply.github.com>
2024-03-20 19:49:47 +0100

    proptypesfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import './App.css';
-import TransportCard from "./components/TransportCard";
+import TransportCard from './components/TransportCard';
 import { useEffect, useState } from 'react';
+import TransportForm from './components/TransportForm';
+
 
 function App() {
   const backend_url="http://localhost:8000/api/public_transports";
@@ -17,12 +19,34 @@ function App() {
     setVehicle(data);
   }
 
-
+  const createVehicle = async () => {
+    const response = await fetch(backend_url, {
+      method: "POST",
+      body: JSON.stringify(vehicle),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    });
+    const data = await response.json();
+    if (response.ok) {
+      readVehicle();
+    } else {
+      alert(data.message);
+    }
+  }
 
   return (<main className='container'>
+    <section>
+      <h2>New Vehicle</h2>
+      <TransportForm onSubmit={createVehicle}/>
+    </section>
+    <section>
+      <h2>Vehicles list:</h2>
     <div className='row row-cols-lg-2 row-cols-1'>
       {vehicle.map(vehicle => <TransportCard vehicle={vehicle} key={vehicle.id}/>)}
     </div>
+    </section>
   </main>);
 }
 
---
+++ src/components/TransportForm.jsx
@@ -0,0 +1,39 @@
+import { useRef } from "react";
+import PropTypes from "prop-types";
+
+function TransportForm(props){
+    const modelRef = useRef(null);
+    const yearRef = useRef(null);
+    const capacityRef = useRef(null);
+    const {onSubmit} = props;
+
+    const createVehicle =() => {
+        const vehicle = {
+            model: modelRef.current.value,
+            year_made: yearRef.current.value,
+            capacity: capacityRef.current.value,
+        };
+        onSubmit(vehicle);
+    }
+    return (<form onSubmit={event => {event.preventDefault(); createVehicle();}}>
+        <div  className="mb-3">
+            <label htmlFor="model" className="form-label">Model</label>
+            <input type="text" className="form-control" id="model" placeholder="Model" ref={modelRef}/>
+        </div>
+        <div  className="mb-3">
+            <label htmlFor="year" className="form-label">Year</label>
+            <input type="date" className="form-control" id="year" placeholder="Year" ref={yearRef}/>
+        </div>
+        <div  className="mb-3">
+            <label htmlFor="capacity" className="form-label">Capayity</label>
+            <input type="number" className="form-control" id="capacity" placeholder="Capacity" ref={capacityRef}/>
+        </div>
+        <button type="submit" className="btn btn-success">Submit</button>
+    </form>);
+}
+
+TransportForm.propTypes ={
+    onSubmit: PropTypes.func.isRequired
+}
+
+export default TransportForm;
